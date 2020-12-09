8bf466a53ef3501d496401c72d8a417a40c9965d
matias-perezferrero <matias.perezferrero@gmail.com>
2020-12-08 20:35:57 -0700

    completed lecture

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './Movie.css'
 
 function Movie(props) {
+    let favorite = props.movie.favorite ? "favorite" : ''
+
     return (
-        <div className="movie-container">
+        <div className={`movie-container ${favorite}`}>
             <h3>{props.movie.name}</h3>
             <p>{props.movie.rating}</p>
             <p>{props.movie.year}</p>
             <p>{props.movie.description}</p>
+            <button onClick={() => props.handleFavorite(props.movie.id)}>Add to Favorites</button>
         </div>
     )
 }
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -8,17 +8,58 @@ class MoviesList extends React.Component {
         super()
 
         this.state = {
+            // [
+            //  {name: "Ratatouille, id: 1, rating: 5, year: 2007", favorite: false},
+            //  {name: "Ratatouille, id: 2, rating: 5, year: 2007"},
+            // ....]
             movies: [],
             isLoading: true
+        }
+
+        this.handleFavorite = this.handleFavorite.bind(this)
+    }
+
+    handleFavorite(id) {
+        console.log('Hit handleFavorite', id)
+        let changedArray = this.state.movies.map(e => {
+            if(e.id === id) {
+                return { ...e, favorite: true }
+            } else {
+                return e
+            }
+        })
+
+        this.setState({
+            movies: changedArray
+        })
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({
+                movies: data,
+                // isLoading: false,
+            })
+        }, 2500)
+
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.isLoading === true) {
+            this.setState({
+                isLoading: false
+            })
         }
     }
 
     render() {
-        let mappedMovies = this.state.movies.map(element => <Movie key={element.id} movie={element}/>)
+        let mappedMovies = this.state.movies.map(element => {
+            return <Movie key={element.id} movie={element} handleFavorite={this.handleFavorite}/>
+        })
 
         return (
             <div className="movies-list-container">
-                {mappedMovies}
+                {this.state.isLoading ? "LOADING..." : mappedMovies}
             </div>
         )
     }
